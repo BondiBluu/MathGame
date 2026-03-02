d370ad93d0556129508c8df890a9cda83533c6ea
Bamidele Ogundeji <developers@previouslypluto.com>
2026-03-02 16:21:31 -0500

    Add project files.

---
+++ MathGame/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+
+namespace MathGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<string>> allTestResults = new List<List<string>>();
+            List<string> singleTestResults = new List<string>();
+
+            int correctAnswerCount = 0;
+            List<string> additionQuestions = new List<string> { 
+                "1 + 1",
+                "1 + 2",
+                "1 + 3",
+                "1 + 4",
+                "1 + 5",
+                "1 + 6"
+            };
+
+            foreach (string question in additionQuestions) {
+                object result = new DataTable().Compute(question, null);
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
